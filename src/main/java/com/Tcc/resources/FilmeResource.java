2d1180d093c164a46e7dcd11f4a71f9c26ddd2e3
Elizia <eliziajs@gmail.com>
2023-01-05 09:38:47 -0300

    Criação da Classe Genero e testes
    Criação da Classe Genero, considerando o Checklist na criação da entidades:
    -Atributos - ok
    -Associações - não
    -Construtores sem incluir coleções no construtor com parâmetros ok
    -Getters e setters ok
    -hashCode e equals somente no id
    -Serializable padrão: 1L (os objetos poderão ser convertidos para uma sequencia de bytes. Para os objetos serem gravados em arquivos, trafegar em rede). ok
    Preferencia em usar um checkList mais tradicional para demonstração de trabalho de conclusão de curso ao invés de usar a biblioteca lombok que enxugaria as linhas de código.

--- src/main/java/com/Tcc/resources/FilmeResource.java
+++ src/main/java/com/Tcc/resources/FilmeResource.java
@@ -1,16 +1,27 @@
 package com.tcc.resources;
 
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.tcc.domain.Genero;
 
 @RestController
 @RequestMapping(value="/filmes")
 public class FilmeResource {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listar() {
-		return "Aleluia";
+	public List<Genero> listar() {
+		
+		Genero gen = new Genero(1, "Drama Policial"); 
+		List<Genero> lista = new ArrayList<>();
+		lista.add(gen);
+		
+		return lista;
 	}
 
 }
