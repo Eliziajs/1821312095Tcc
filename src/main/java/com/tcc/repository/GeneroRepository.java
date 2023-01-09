438fd319f883aecdab25604b91ab8e3ffa2b91c3
Elizia <eliziajs@gmail.com>
2023-01-09 17:21:58 -0300

    Implementação das camadas REST
    Implementação REST com o Banco de dado com a criação das empresas Service, Repository e Resources (Controller) e teste

---
+++ src/main/java/com/tcc/repository/GeneroRepository.java
@@ -0,0 +1,8 @@
+package com.tcc.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.tcc.domain.Genero;
+
+public interface GeneroRepository extends JpaRepository<Genero, Integer> {
+
+}
---
+++ src/main/java/com/tcc/resources/GeneroResource.java
@@ -0,0 +1,30 @@
+package com.tcc.resources;
+
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcc.domain.Genero;
+import com.tcc.services.GeneroService;
+
+
+@RestController
+@RequestMapping(value="/generos")
+public class GeneroResource {
+	
+	@Autowired
+	private GeneroService service;
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<?> find(@PathVariable Integer id) {
+		Optional<Genero> obj = service.buscar(id);
+		return ResponseEntity.ok().body(obj);
+
+	}
+}
